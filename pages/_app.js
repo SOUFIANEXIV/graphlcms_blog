a24374814d85c2324f89d1964063987f89a451de
SOUFIANEXIV <98940737+SOUFIANEXIV@users.noreply.github.com>
2022-12-02 13:37:55 +0100

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,32 @@ import '../styles/globals.scss'
 import React from 'react'
 import { Layout } from '../components'
 import ScrollToTop from '../components/ScrollToTop'
+import { useEffect } from 'react'
+import { pageview } from "../lib/gtag";
+
+import { useRouter } from 'next/router'
+
+
 
 
 
 
 function MyApp({ Component, pageProps }) {
-  
+
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+
 
   return(
   
